18d219f48b9931018dca530e149d66ab763639b9
vortex2wenty <baseballmario101@gmail.com>
2021-08-08 08:13:52 -0400

    Work on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,25 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <Router>
+      <header>
+        <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+              <li>
+                <Link to="/resume">Resume</Link>
+              </li>
+            </ul>
+        </nav>
+      </header>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/projects" component={Home}/>
+        <Route path="/resume" component={Home}/>
       </Switch>
     </Router>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Avatar from '../Avatar.svg';
 
 const Home = () => {
-	return (
-		<h1>Home</h1>
-	);
+  return (
+    <main>
+      <section className="hero">
+        <h1 className="hero-title">Hi, I'm Nathan.</h1>
+        <figure>
+          <img className="hero-img" src={Avatar} alt="My avatar." />
+          <figcaption>An aspiring full-stack web developer.</figcaption>
+        </figure>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
